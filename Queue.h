8125e466d81ff96f21d32662b87e19cbf41caf0f
Mourad Rashwan <32272179+MouradRashwan@users.noreply.github.com>
2019-05-23 11:30:22 +0200

    Add files via upload

---
+++ Queue.h
@@ -0,0 +1,40 @@
+#ifndef QUEUE_H_INCLUDED
+#define QUEUE_H_INCLUDED
+
+typedef struct Queue
+{
+    int32_t i32Head;
+    int32_t i32Tail;
+    int32_t i32QueueLen;
+    int32_t i32ActualLen;
+    int32_t i32ElementSize;
+    int32_t i32Next;
+    bool bHeadingToTail;
+    void *pvArray;
+} Queue_t;
+
+void Queue_allocateStatic(Queue_t *ptQueue, void *pvQueueArray, int32_t i32QueueLen, int32_t i32ElementSize);
+
+bool Queue_allocateDynamic(Queue_t *ptQueue, int32_t i32QueueLen, int32_t i32ElementSize);
+
+void Queue_deallocateDynamic(Queue_t *ptQueue);
+
+bool Queue_isEmpty(Queue_t *ptQueue);
+
+bool Queue_isFull(Queue_t *ptQueue);
+
+int32_t Queue_getActualLen(Queue_t *ptQueue);
+
+int32_t Queue_getQueueLen(Queue_t *ptQueue);
+
+bool Queue_getHead(Queue_t *ptQueue, void *pvElement);
+
+bool Queue_getTail(Queue_t *ptQueue, void *pvElement);
+
+bool Queue_getNext(Queue_t *ptQueue, void *pvElement);
+
+bool Queue_dequeue(Queue_t *ptQueue, void *pvElement);
+
+bool Queue_enqueue(Queue_t *ptQueue, void *pvElement);
+
+#endif // QUEUE_H_INCLUDED
---
+++ Queue_example.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+#include "Queue.h"
+
+#define QUEUE_LENGTH                5
+#define QUEUE_STATIC_ALLOCATION     0
+
+typedef struct Object
+{
+    int32_t i32Val1;
+    int32_t i32Val2;
+} Object_t;
+
+int main(void)
+{
+    printf("Queue Starting ...\n");
+
+    int32_t ii;
+    Queue_t tQueue;
+    Object_t tObject;
+
+#if(QUEUE_STATIC_ALLOCATION == 0)
+    if(Queue_allocateDynamic(&tQueue, QUEUE_LENGTH, sizeof(Object_t)) == false)
+    {
+        printf("<<ERROR>> Heap Memory is Full");
+    }
+#else
+    static Object_t atObject[QUEUE_LENGTH];
+    Queue_allocateStatic(&tQueue, atObject, QUEUE_LENGTH, sizeof(Object_t));
+#endif
+
+    for(ii=0; ii<QUEUE_LENGTH; ii++)
+    {
+        tObject.i32Val1 = ii + 1;
+        tObject.i32Val2 = (ii + 1) * 10;
+        Queue_enqueue(&tQueue, &tObject);
+    }
+
+    Queue_dequeue(&tQueue, &tObject);
+    tObject.i32Val1 = 6;
+    tObject.i32Val2 = 60;
+    Queue_enqueue(&tQueue, &tObject);
+
+    Queue_getHead(&tQueue, &tObject);
+    printf("(%u|%u) ", tObject.i32Val1, tObject.i32Val2);
+    while(Queue_getNext(&tQueue, &tObject))
+    {
+        printf("(%u|%u) ", tObject.i32Val1, tObject.i32Val2);
+    }
+    printf("\n\n");
+
+    Queue_getTail(&tQueue, &tObject);
+    printf("(%u|%u) ", tObject.i32Val1, tObject.i32Val2);
+    while(Queue_getNext(&tQueue, &tObject))
+    {
+        printf("(%u|%u) ", tObject.i32Val1, tObject.i32Val2);
+    }
+    printf("\n\n");
+
+    return 0;
+}
